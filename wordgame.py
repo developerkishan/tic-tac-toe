5433c740ac3995aecd84cdeccc52486451f37307
Kishan Agarwalla <kiagarwa@akamai.com>
2024-12-04 06:55:42 +0530

    refined-code

--- wordgame.py
+++ wordgame.py
@@ -1,5 +1,4 @@
 import random
-
 WELCOME_MESSGE = "Welcome to the Word Guessing Game!"
 GOOD_GUESS_MESSAGE = "Good guess!"
 INVALID_INPUT_MESSAGE = "Please enter a valid alphabet"
@@ -9,12 +8,9 @@ INCORRECT_GUESS_MESSAGE = "Oops! That letter is not in the word."
 GAME_WON_MESSAGE = "Congratulations! You guessed the word:"
 GAME_OVER_MESSAGE = "Game over! The word was:"
 REMAINING_ATTEMPTS_MESSAGE = "Remaining attempts:"
-
-
 def print_status(attempts,display_word):
     print(f"{REMAINING_ATTEMPTS_MESSAGE} {attempts}" )
     print(' '.join(display_word))
-
 def is_valid_input(guessed_char , guessed_chars):
     if not  guessed_char.isalpha():
         print(INVALID_INPUT_MESSAGE)
@@ -28,12 +24,10 @@ def is_game_won(display_word,secret_word):
     if ''.join(display_word) == secret_word:
         return True
     return False
-
 def main():
     words = ['badminton', 'coding', 'business', 'coach', 'sports', 'speaker']
     secret_word = random.choice(words)
     secret_word_dict = dict(enumerate(secret_word))
-
     guessed_chars = set()
     attempts = 8
     print(WELCOME_MESSGE)
@@ -43,16 +37,14 @@ def main():
         guessed_char = input("Player inputs: ").lower()
         if not is_valid_input(guessed_char,guessed_chars):
             continue
-        is_found = 'no'
-        #i want to look on the basis of the dictionary 
-        keys = [key for key,value in secret_word_dict.items() if guessed_char == value]
-        if keys == []:
+        matching_indices = [key for key,value in secret_word_dict.items() if guessed_char == value]
+        if matching_indices == []:
             print(INCORRECT_GUESS_MESSAGE)
             attempts-=1
             print_status(attempts,display_word)
         else:
             print(GOOD_GUESS_MESSAGE)
-            for key in keys:
+            for key in matching_indices:
                 display_word[key]= guessed_char
                 print_status(attempts,display_word)
         if is_game_won(display_word,secret_word):
@@ -60,6 +52,5 @@ def main():
             break
     if attempts ==0:
         print(f"{GAME_OVER_MESSAGE} {secret_word}")
-
 if __name__ == "__main__":
     main()
